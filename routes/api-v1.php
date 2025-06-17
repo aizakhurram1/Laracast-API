3ace1554c1ee21a09ef605fa1dd5a3dc8ca49253
aizakhurram1 <aiza.khurram@dubizzlelabs.com>
2025-06-17 17:26:19 +0500

    using & filtering nested resources

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthorsController;
+use App\Http\Controllers\Api\V1\AuthorTicketsController;
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\Api\Authcontroller;
-use App\Http\Controllers\Api\V1\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Ticket;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,4 +13,5 @@ Route::get('/user', function (Request $request) {
 
 //Route::resource('tickets',TicketController::class);
 Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
-Route::middleware('auth:sanctum')->apiResource('users', UsersController::class);
+Route::middleware('auth:sanctum')->apiResource('authors', AuthorsController::class);
+Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketsController::class);
